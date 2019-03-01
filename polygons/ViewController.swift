933d0efae4e68c5e37be3aefbcb9fa90fd566d7b
Mac Mini Novo <macmininew@Mac-mini-de-Mac.local>
2019-03-01 11:35:58 -0300

    feature/grip-component Add grid.
    -
    
    Signed-off-by: Mac Mini Novo <macmininew@Mac-mini-de-Mac.local>

--- polygons/ViewController.swift
+++ polygons/ViewController.swift
@@ -20,8 +20,13 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        let tapGR = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(draw))
         view.addGestureRecognizer(tapGR)
+        
+//        let tapGR = UITapGestureRecognizer(target: self, action: #selector(tapped))
+//        view.addGestureRecognizer(tapGR)
+
     }
     
     @objc func tapped(gr:UITapGestureRecognizer) {
@@ -37,5 +42,27 @@ class ViewController: UIViewController {
         demoView.array = array
         self.view.addSubview(demoView)
     }
+    
+    @objc func draw(gr:UITapGestureRecognizer) {
+        let loc:CGPoint = gr.location(in: gr.view)
+        
+        let positionX = loc.x
+        let positionY = loc.y
+
+        let x: CGFloat = CGFloat(Int((positionX - 23) / 50))
+        let xToUse: CGFloat = 23 + (x * 50)
+        
+        var y: CGFloat = CGFloat(Int((positionY - 170) / 50))
+        let yToUse: CGFloat = 170 + (y * 50)
+
+        let width: CGFloat = 50
+        let height: CGFloat = 50
+        
+        let customView = UIView(frame: CGRect(x: xToUse, y: yToUse, width: width, height: height))
+        customView.backgroundColor = UIColor.red
+        customView.alpha = 0.5
+        self.view.addSubview(customView)
+    }
+    
 }
 
